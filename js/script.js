5b85e95f4df138a462a0a5172ec83cc071fededb
Alina2495 <48183610+Alina2495@users.noreply.github.com>
2021-05-19 18:09:05 +0300

    19.05.2021

--- js/script.js
+++ js/script.js
@@ -32,3 +32,34 @@ var swiper = new Swiper('.swiper-news', {
         }
     },
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 0
+
+        }
+    },
+});
