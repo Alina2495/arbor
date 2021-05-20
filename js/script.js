07340d1b8c6ee2b9c03937d791d617035b82c1e5
Alina2495 <48183610+Alina2495@users.noreply.github.com>
2021-05-20 17:54:47 +0300

    20.05.2021

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ var swiper = new Swiper('.swiper-news', {
 });
 
 
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-production', {
     slidesPerView: 4,
     spaceBetween: 30,
     loop: true,
@@ -62,4 +62,40 @@ var swiper = new Swiper('.swiper-container', {
 
         }
     },
+});
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 8,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        // 320: {
+        //     slidesPerView: 1,
+        //     spaceBetween: 0
+        // },
+        // 480: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20
+        // },
+        768: {
+            // slidesPerView: 4,
+            spaceBetween: 16
+        },
+        // 1400: {
+        //     slidesPerView: 5,
+        //     spaceBetween: 0
+        // }
+    },
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
 });
