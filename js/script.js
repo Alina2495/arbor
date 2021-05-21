fd76fb36e0491cd74d4924ddb0b9bc67c606d4e2
Alina2495 <48183610+Alina2495@users.noreply.github.com>
2021-05-21 18:08:06 +0300

    21.05.2021

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ var swiper = new Swiper('.swiper-production', {
     },
 });
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
+var galleryThumbs = new Swiper('.new-gallery-thumbs', {
     spaceBetween: 8,
     slidesPerView: 4,
     freeMode: true,
@@ -81,7 +81,7 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
         // },
         768: {
             // slidesPerView: 4,
-            spaceBetween: 16
+            spaceBetween: 11
         },
         // 1400: {
         //     slidesPerView: 5,
@@ -89,7 +89,7 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
         // }
     },
 });
-var galleryTop = new Swiper('.gallery-top', {
+var galleryTop = new Swiper('.new-gallery-top', {
     spaceBetween: 10,
     navigation: {
         nextEl: '.swiper-button-next',
@@ -97,5 +97,45 @@ var galleryTop = new Swiper('.gallery-top', {
     },
     thumbs: {
         swiper: galleryThumbs
+    }
+});
+
+
+
+// swiper slider (only card page)
+var galleryThumbs = new Swiper('.card-gallery-thumbs', {
+    direction: 'vertical',
+    spaceBetween: 6,
+    slidesPerView: 3,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 3
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 3
+        },
+        // when window width is >= 640px
+        992: {
+            spaceBetween: 15
+        },
+        // when window width is >= 1200px
+        1200: {
+            spaceBetween: 10
+        }
+    }
+});
+var galleryTop = new Swiper('.card-gallery-top', {
+    direction: 'vertical',
+    spaceBetween: 10,
+    thumbs: {
+        swiper: galleryThumbs,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     }
 });
